a98fc39af4430bdae47b866fabb2501e0404e22c
u1642 <42061032+u1642@users.noreply.github.com>
2022-11-08 17:01:53 +0100

    Initial commit

---
+++ R/Authorization.R
@@ -0,0 +1,13 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Get Metering Point Information
+#'
+#' This function is not finished yet, but should get all the meering point that i have acess to
+#' I haven testet that correct data is returned when retrieving multiple meteringPointID, since I have only access to one
+#'
+#' @param infile Path to the input file
+#' @return A matrix of the infile
---
+++ R/GlobalVariables.R
@@ -0,0 +1,20 @@
+
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Get Metering Point Information
+#'
+#' This function is not finished yet, but should get all the meering point that i have acess to
+#' I haven testet that correct data is returned when retrieving multiple meteringPointID, since I have only access to one
+#'
+#'
+#'
+#'
+#'
+#' Global variables
+
+pkg.env <- new.env(parent = emptyenv())
+pkg.env$andelenergi.url <- "https://api.eloverblik.dk"
