85e63a62c50047dd88a7bdf5b041915e2dfc45e3
u1642 <42061032+u1642@users.noreply.github.com>
2022-11-12 11:41:08 +0100

    Update MeterData.R
    Added vonverting from json til a list

--- R/MeterData.R
+++ R/MeterData.R
@@ -10,22 +10,20 @@
 #' I haven testet that correct data is returned when retrieving multiple meteringPointID, since I have only access to one
 #'
 #'
-#' @param data_access_token
-#' @return JSON
+#' @param data_access_token The data_access_token i provided bye get_data_access_token(refreshtoken)
+#' @return List the function return a list of objects taht contain datapoint information
 #' @export
 
 getMeteringPointInfo <-
   function(data_access_token) {
     path <-
       "/customerapi/api/meteringpoints/meteringpoints?includeAll=true"
-    httr::GET(
+    raw_metering_point_info <- httr::GET(
       url = pkg.env$andelenergi.url,
       path = path,
       httr::accept_json(),
       httr::add_headers(Authorization = paste("Bearer ", data_access_token))
     )
+    jsonlite::fromJSON(rawToChar(raw_metering_point_info$content))$result
   }
-
-
-
 
