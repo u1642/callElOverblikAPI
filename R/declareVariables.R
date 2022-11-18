68c19368721e3a53ca87c4df8fed075d518d560b
u1642 <42061032+u1642@users.noreply.github.com>
2022-11-19 00:31:48 +0100

    Update declareVariables.R
    Update globel variable

--- R/declareVariables.R
+++ R/declareVariables.R
@@ -1,20 +1,69 @@
-
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-#' Get Metering Point Information
-#'
-#' This function is not finished yet, but should get all the meering point that i have acess to
-#' I haven testet that correct data is returned when retrieving multiple meteringPointID, since I have only access to one
-#'
-#'
-#'
-#'
-#'
 #' Global variables
-
 pkg.env <- new.env(parent = emptyenv())
 pkg.env$andelenergi.url <- "https://api.eloverblik.dk"
+
+
+# out_Quantity.quality
+A01 <- c(
+  "A01",
+  "Korrigeret",
+  "Adjusted",
+  "Will no longer be used after February 2020. Until then it specifies energy quantities 13/13 Doc.19/11830-2 Offentlig/public which are calculated by Dat-aHub."
+)
+
+A02 <- c(
+  "A02",
+  "Mangler",
+  "Not available",
+  "Specifies that the grid oper-ator has submitted a “miss-ing indicator” to DataHub for the specific position, meaning that the energy quantity is not available. Therefore, no quantity will be returned for the specific position."
+)
+
+A03 <- c(
+  "A03",
+  "Estimeret",
+  "Estimated",
+  "Specifies that the grid oper-ator has submitted the quantity to DataHub as an estimate."
+)
+
+A04 <- c(
+  "A04",
+  "Målt",
+  "As provided",
+  "Specifies that the grid oper-ator has submitted the quantity to DataHub as measured."
+)
+
+
+A05 <- c(
+  "A05",
+  "Ufuldstændig",
+  "Incomplete",
+  "Is applied to an aggregated energy quantity if at least one of the quantities in-cluded in the aggregation has been submitted to Data-Hub with a “missing indica-tor”, meaning that the quantity is not available (as described under code A02).
+"
+)
+
+Quantity.quality <- as.data.frame(rbind(c(A01),c(A02),c(A03),c(A04),c(A05)))
+
+pkg.env$Quantity.quality <- Quantity.quality
+
+
+pkg.env$dataframeElOverblik <-
+  function() {
+    df <- stats::setNames(
+      data.frame(matrix(ncol = 7, nrow = 0)),
+      c(
+        "meter",
+        "start",
+        "end",
+        "position",
+        "resolution",
+        "quantity",
+        "quality"
+      )
+    )
+    df$start <- as.POSIXct(df$start)
+    df$end <- as.POSIXct(df$end)
+
+    # Return dataframe template
+    df
+  }
+
