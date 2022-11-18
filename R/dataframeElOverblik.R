7b6f1084e934ebbfbcf11fce40326075a8b233e8
u1642 <42061032+u1642@users.noreply.github.com>
2022-11-19 00:31:24 +0100

    Create dataframeElOverblik.R
    Return global variable

---
+++ R/dataframeElOverblik.R
@@ -0,0 +1,15 @@
+#' getTimeSeriesData
+#'
+#' This set of functions is handling Token
+#'
+#'
+#' @description This function retrieves timesere data from ElOverblik
+#'
+#' @return dataframe Function returns dataframe, the DF doen contains but ensures at al modules uses the same FD
+#' @export
+dataframeElOverblik <-
+  function() {
+    pkg.env$dataframeElOverblik
+  }
+
+
