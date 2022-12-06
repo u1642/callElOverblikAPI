cbfc2b34e4803e0a9f4290925239eeaabfe79ad5
u1642 <42061032+u1642@users.noreply.github.com>
2022-12-06 17:51:13 +0100

    Update getIsAlive.R

--- R/getIsAlive.R
+++ R/getIsAlive.R
@@ -1,13 +1,12 @@
 #' Get Is Alive
 #'
-#' Checks if ElOverblik endpoint is alive
+#' Checks if ElOverblik endpoint is alive.
+#' If we are experiencing more traffic than we can handle, this endpoint may return HTTP 503. The status currently refreshes every 60 seconds.
 #'
 #'
-#' @examples
-#' #bmi.vals <- rnorm(n = 50, mean = 25, sd = 3)
 #'
 #' @export
-#'
+
 getIsAlive <- function() {
   url <- pkg.env$andelenergi.url
   path <- "/customerapi/api/isalive"
@@ -17,5 +16,11 @@ getIsAlive <- function() {
       url = url,
       path = path)
 
-  rawToChar(endpoinntIsAlive$content)
+  if (endpoinntIsAlive$status_code == "200") {
+    rawToChar(endpoinntIsAlive$content)
+  } else {
+    print(sprintf("Error code: %d",endpoinntIsAlive$status_code))
+  }
+
+
   }
