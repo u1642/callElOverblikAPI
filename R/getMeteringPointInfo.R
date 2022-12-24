4316d41876c318f9a4eabdb9d5a0e4c96315ee75
u1642 <42061032+u1642@users.noreply.github.com>
2022-12-24 18:30:51 +0100

    Update getMeteringPointInfo.R

--- R/getMeteringPointInfo.R
+++ R/getMeteringPointInfo.R
@@ -17,18 +17,14 @@
 #' * \[settlementMethod\]
 #' * \[meterReadingOccurrence\]
 #' * \[meterNumber\]
-#' * \[meterNumber\]
 #' * \[consumerStartDate\]
 #' @md
-#'
-#'
 #' meteringPointId,typeOfMP,balanceSupplierName,postcode,cityName,hasRelation,consumerCVR,dataAccessCVR,childMeteringPoints)
 #'
 #'
 #'
-#'
 #' @param data_access_token The data_access_token i provided by get_data_access_token(refreshtoken)
-#' @return A list containing
+#' @return list A list containing all the basic information for the metering point
 #' @export
 
 getMeteringPointInfo <-
@@ -43,4 +39,3 @@ getMeteringPointInfo <-
     )
     jsonlite::fromJSON(rawToChar(raw_metering_point_info$content))$result
   }
-
