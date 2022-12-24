80c896a3abe586eb9dd888b9839023fbe0603a6f
u1642 <42061032+u1642@users.noreply.github.com>
2022-12-24 15:38:21 +0100

    Create getMesuringQuality.R

---
+++ R/getMesuringQuality.R
@@ -0,0 +1,24 @@
+#' getMesuringQuality
+#'
+#' This function translate from the quality code A1 .. A5 to text
+#' Code = A01 DK translation = Korrigeret UK translation = Adjusted Tekst =  Korrigeret
+#'
+#'
+#' @description This functionen
+#'
+#' @param core The quality code from the metering observation
+#' @param lang The language you want returned e.g. DK or UK, default = UK
+#' @return text Translated quality code
+#' @export
+getMesuringQuality <- function(code, lang = "UK") {
+  # Return the quality text
+  index = (Quantity.quality$Code == code)
+  result = Quantity.quality[index,lang]
+
+  if(length(result)) {
+    return(result)
+  } else
+  {
+    return("ERROR in lookup")
+  }
+}
