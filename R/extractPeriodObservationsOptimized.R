96f0c5f19f360251f7f6a944822d9e2d9a64eaff
u1642 <42061032+u1642@users.noreply.github.com>
2022-12-25 18:05:23 +0100

    Create extractPeriodObservationsOptimized.R
    Moving functionality out in functions
    Will try to get rid of the for loops

---
+++ R/extractPeriodObservationsOptimized.R
@@ -0,0 +1,49 @@
+#' extractPeriodObservationsOptimized
+#'
+#' DO NOT USE THIS FUNCTION, I's for test use only
+#' This set of functions extract the time serie data recieved from ger time serie data
+#'
+#'
+#' @description DON'T USE THIS FUNCTION
+#'
+#' @param meter This is your meter number identifying your meter e.g. "123456789012356789"
+#' @param period This is the period fetched from getTimeSerie
+#' @return dataframe Returns dataframe with observations
+#' @export
+extractPeriodObservationsOptimized <- function(meter, period) {
+  observation <- dataframeElOverblik()
+
+  for (d in period) {
+    getBasicData(d)
+
+
+    for (p in d$Point) {
+      getObservation(p)
+
+    }
+  }
+  observation
+}
+
+
+getBasicData <- function(d) {
+  start_date <-
+    as.POSIXct(d$timeInterval$start, format = "%Y-%m-%dT%H:%M:%OSZ", tz = "UTC")
+  end_date <-
+    as.POSIXct(d$timeInterval$end, format = "%Y-%m-%dT%H:%M:%OSZ", tz = "UTC")
+
+  resolution <- d$resolution
+}
+
+getObservation <- function(p) {
+  point_observation <-
+    data.frame(
+      meter,
+      start_date,
+      end_date,
+      p$position,
+      resolution,
+      p$out_Quantity.quantity,
+      p$out_Quantity.quality
+    )
+}
